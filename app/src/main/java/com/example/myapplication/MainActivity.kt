54616fa3a54dcedc6e297736c1021bdccb75a703
Vaibhav Chopade <vaibhavchopade33@gmail.com>
2019-12-05 18:15:24 +0530

    Time module working but have issue of Local Time, unable to set Previous time

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -19,16 +19,20 @@ class MainActivity : AppCompatActivity() {
 
         val mPickInTimeBtn = findViewById<Button>(R.id.pickInTimeBtn)
         val textViewInTime = findViewById<TextView>(R.id.textViewInTime)
-        //val mPickOutTimeBtn = findViewById<Button>(R.id.pickOutTimeBtn)
-        //val textViewOutTime = findViewById<TextView>(R.id.textViewOutTime)
+
         val submitBtn = findViewById<Button>(R.id.buttonSubmit)
         val timeCompletedTextView = findViewById<TextView>(R.id.textViewTimeCompleted)
         val timeRemf8TextView = findViewById<TextView>(R.id.textViewTimeRemf8)
         val timeRemf9TextView = findViewById<TextView>(R.id.textViewTimeRemf9)
 
         val timeCompletedHomeTextView = findViewById<TextView>(R.id.textViewHomeTimeCompleted)
+        val timeCompletedHomeMinTextView = findViewById<TextView>(R.id.textViewHomeMinTimeCompleted)
+
         val timeRemf8HomeTextView = findViewById<TextView>(R.id.textViewHomeTimeRemf8)
+        val timeRemf8HomeMinTextView = findViewById<TextView>(R.id.textViewHomeMinTimeRemf8)
+
         val timeRemf9HomeTextView = findViewById<TextView>(R.id.textViewHomeTimeRemf9)
+        val timeRemf9HomeMinTextView = findViewById<TextView>(R.id.textViewHomeMinTimeRemf9)
 
         mPickInTimeBtn.setOnClickListener {
             val cal = Calendar.getInstance()
@@ -47,22 +51,6 @@ class MainActivity : AppCompatActivity() {
             ).show()
         }
 
-        /*mPickOutTimeBtn.setOnClickListener {
-            val cal = Calendar.getInstance()
-            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
-                cal.set(Calendar.HOUR_OF_DAY, hour)
-                cal.set(Calendar.MINUTE, minute)
-                textViewOutTime.text = SimpleDateFormat("HH:mm").format(cal.time)
-            }
-            TimePickerDialog(
-                this,
-                timeSetListener,
-                cal.get(Calendar.HOUR_OF_DAY),
-                cal.get(Calendar.MINUTE),
-                true
-            ).show()
-        }*/
-
         submitBtn.setOnClickListener {
 
             val format =
@@ -70,10 +58,18 @@ class MainActivity : AppCompatActivity() {
             val localTime = LocalTime()
             var d1: Date? = null
             var d2: Date? = null
+
+            var d3: Date? = null
+            var d4: Date? = null
+
             Toast.makeText(this@MainActivity, localTime.toString() , Toast.LENGTH_SHORT).show()
             try {
                 d1 = format.parse(textViewInTime.text.toString())
                 d2 = format.parse(localTime.toString())
+
+                d3 = format.parse(localTime.plusHours(9).toString())
+                d4 = format.parse(localTime.plusHours(8).toString())
+
                 val dt1 = DateTime(d1)
                 val dt2 = DateTime(d2)
                 //timeCompletedTextView.text = Minutes.minutesBetween(dt1, dt2).getMinutes() .toString()
@@ -81,7 +77,10 @@ class MainActivity : AppCompatActivity() {
                 val hours = t / 60 //since both are ints, you get an int
                 val minutes = t % 60
                 timeCompletedTextView.text = hours.toString() + "hr " + minutes.toString() + "min"
-                timeCompletedHomeTextView.text =  SimpleDateFormat("HH:mm").format(d1)
+                timeCompletedHomeTextView.text =  SimpleDateFormat("HH").format(d2)
+                timeCompletedHomeMinTextView.text = SimpleDateFormat("mm").format(d2)
+
+
 
                 var temT = 540 - t
                 val temhours = temT / 60
@@ -95,6 +94,18 @@ class MainActivity : AppCompatActivity() {
                 timeRemf8TextView.text = temphours.toString() + "hr " + tempminutes.toString() + "min"
 
 
+                //val d3 = LocalTime().plusHours(8)
+                //val d4 = LocalTime().plusHours(9)
+
+                Toast.makeText(this@MainActivity, d3.toString() , Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, SimpleDateFormat("HH:mm").format(d3).toString() , Toast.LENGTH_SHORT).show()
+
+                timeRemf9HomeTextView.text =  SimpleDateFormat("HH").format(d3)
+                timeRemf9HomeMinTextView.text = SimpleDateFormat("mm").format(d3)
+
+                timeRemf8HomeTextView.text = SimpleDateFormat("HH").format(d4)
+                timeRemf8HomeMinTextView.text = SimpleDateFormat("mm").format(d4)
+
                 //val r = Integer.parseInt(t.toString())
                 Toast.makeText(this@MainActivity, minutes.toString() , Toast.LENGTH_SHORT).show()
 
@@ -102,13 +113,6 @@ class MainActivity : AppCompatActivity() {
             } catch (e: Exception) {
                 e.printStackTrace()
             }
-
-
-
-
-
-
-
 
         }
     }
