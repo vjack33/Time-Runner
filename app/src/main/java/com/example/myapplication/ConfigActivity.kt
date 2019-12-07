0622a4ace6cb3f28513a3e9ec941ed64e3fa2328
Vaibhav Chopade <vaibhavchopade33@gmail.com>
2019-12-07 23:35:07 +0530

    Working with Setting Save and Button null Fixed. no Crash now

--- app/src/main/java/com/example/myapplication/ConfigActivity.kt
+++ app/src/main/java/com/example/myapplication/ConfigActivity.kt
@@ -1,8 +1,11 @@
 package com.example.myapplication
 
 import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceFragment
+import android.telephony.mbms.MbmsErrors
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_config.*
@@ -15,74 +18,71 @@ class ConfigActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_config)
 
-        val sharedPreferences = getSharedPreferences("SP_INFO", Context.MODE_PRIVATE)
+        val sharedPreferences = getSharedPreferences("SP_INFO", Context.MODE_PRIVATE)            //Initialization of SharedPreferences for storing settings
 
+        // If Settings is NULL : When settings opened for first time
         if(sharedPreferences.getString("NAME","") != null){
 
-            editTextConfigName.hint = sharedPreferences.getString("NAME","")
             editTextConfigName.setText(sharedPreferences.getString("NAME",""))
-
             checkBoxSunday.isChecked = sharedPreferences.getBoolean("SUNDAY",false)
             checkBoxMonday.isChecked = sharedPreferences.getBoolean("MONDAY", false)
-/*            checkBoxTuesday.isChecked = sharedPreferences.getBoolean("TUESDAY",false)
+            checkBoxTuesday.isChecked = sharedPreferences.getBoolean("TUESDAY",false)
             checkBoxWednesday.isChecked = sharedPreferences.getBoolean("WEDNESDAY",false)
             checkBoxThursday.isChecked = sharedPreferences.getBoolean("THURSDAY",false)
             checkBoxFriday.isChecked = sharedPreferences.getBoolean("FRIDAY",false)
             checkBoxSunday.isChecked = sharedPreferences.getBoolean("SATURDAY", false)
-
             editTextMaxHours.setText(sharedPreferences.getString("MAXHOURS",""))
             editTextMaxMinutes.setText(sharedPreferences.getString("MAXMINUTES", ""))
             editTextMinHours.setText(sharedPreferences.getString("MINHOURS",""))
             editTextMinMinutes.setText(sharedPreferences.getString("MINMINUTES",""))
             editTextWeekHours.setText(sharedPreferences.getString("WEEKHOURS",""))
-            editTextWeekMinutes.setText(sharedPreferences.getString("WEEKMINUTES",""))*/
+            editTextWeekMinutes.setText(sharedPreferences.getString("WEEKMINUTES",""))
         }
 
+        // Action to do on pressing the Save Button on Configuration screen
         buttonConfigSave.setOnClickListener {
 
             val settingName = editTextConfigName.text.toString()
-
             val settingSunday = checkBoxSunday.isChecked
             val settingMonday = checkBoxMonday.isChecked
-/*            val settingTuesday = checkBoxTuesday.isChecked
+            val settingTuesday = checkBoxTuesday.isChecked
             val settingWednesday = checkBoxWednesday.isChecked
             val settingThursday = checkBoxThursday.isChecked
             val settingFriday = checkBoxFriday.isChecked
             val settingSaturday = checkBoxSaturday.isChecked
-
             val settingMaxHours = editTextMaxHours.text.toString()
             val settingMaxMinutes = editTextMaxMinutes.text.toString()
             val settingMinHours = editTextMinHours.text.toString()
             val settingMinMinutes = editTextMinMinutes.text.toString()
             val settingWeekHours = editTextWeekHours.text.toString()
-            val settingWeekMinutes = editTextWeekMinutes.text.toString()*/
+            val settingWeekMinutes = editTextWeekMinutes.text.toString()
 
             val editor = sharedPreferences.edit()
             editor.putString("NAME",settingName)
             editor.putBoolean("SUNDAY", settingSunday)
             editor.putBoolean("MONDAY", settingMonday)
-/*            editor.putBoolean("TUESDAY", settingTuesday)
+            editor.putBoolean("TUESDAY", settingTuesday)
             editor.putBoolean("WEDNESDAY", settingWednesday)
             editor.putBoolean("THURSDAY", settingThursday)
             editor.putBoolean("FRIDAY", settingFriday)
             editor.putBoolean("SATURDAY", settingSaturday)
-
             editor.putString("MAXHOURS", settingMaxHours)
             editor.putString("MAXMINUTES", settingMaxMinutes)
             editor.putString("MINHOURS", settingMinHours)
             editor.putString("MINMINUTES", settingMinMinutes)
             editor.putString("WEEKHOURS", settingWeekHours)
-            editor.putString("WEEKMINUTES", settingWeekMinutes)*/
-
+            editor.putString("WEEKMINUTES", settingWeekMinutes)
             editor.apply()
 
+            //Start Main activity after saving the Configs
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
 
+        // Cancel Button onclick Function
         buttonConfigCancel.setOnClickListener {
-            val name = sharedPreferences.getString("NAME","")
-            val sunday = sharedPreferences.getBoolean("SUNDAY", false)
-            val monday = sharedPreferences.getBoolean("MONDAY", false)
-            Toast.makeText(this@ConfigActivity, name.toString() + sunday.toString() + monday.toString() +"You clicked me.", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.myapplication
 
 import android.app.TimePickerDialog
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 import java.text.SimpleDateFormat
+import java.time.LocalDate
 import java.time.LocalTime
 import java.time.temporal.ChronoUnit
 import java.util.*
@@ -18,6 +23,8 @@ class MainActivity : AppCompatActivity() {
 /*
         Initialization of every TextView and Buttons
 */
+        textViewToday.text = LocalDate.now().dayOfWeek.toString()                                       // Get today's day eg: SUNDAY
+
         val mPickInTimeBtn = findViewById<Button>(R.id.pickInTimeBtn)                                   // Select Time Button Init
         val textViewInTime = findViewById<TextView>(R.id.textViewInTime)                                // Main Time Entry Text Init
         val submitBtn = findViewById<Button>(R.id.buttonSubmit)                                         // Submit Button Init
@@ -63,63 +70,90 @@ class MainActivity : AppCompatActivity() {
         }
 
         submitBtn.setOnClickListener {
+
+            if(textViewInTime.text == "Time"){
+                Toast.makeText(this@MainActivity, "Please select a time.", Toast.LENGTH_SHORT).show()
+            }
+            else {
 /*
             Initial textView Colors
 */
-            timeCompletedTextView.setTextColor(getColor(R.color.black))
-            timeRemf9TextView.setTextColor(getColor(R.color.black))
-            timeRemf8TextView.setTextColor(getColor(R.color.black))
 
-            hourLocal = LocalTime.now().hour
-            minuteLocal = LocalTime.now().minute
+                timeCompletedTextView.setTextColor(getColor(R.color.black))
+                timeRemf9TextView.setTextColor(getColor(R.color.black))
+                timeRemf8TextView.setTextColor(getColor(R.color.black))
+
+                hourLocal = LocalTime.now().hour
+                minuteLocal = LocalTime.now().minute
 /*
             Initializing the four different time values for many states
 */
-            var d1 : LocalTime = LocalTime.parse(textViewInTime.text.toString())                                        // Parse Time selector text as Time
-            var d2 : LocalTime = LocalTime.now()                                                                        // Get local time from device
-            var d3: LocalTime = d1.plusHours(9)                                                             // Set Time plus 9 hr
-            var d4: LocalTime = d1.plusHours(8)                                                             // Set Time plus 8 hr
+
+                var d1: LocalTime =
+                    LocalTime.parse(textViewInTime.text.toString())                                        // Parse Time selector text as Time
+                var d2: LocalTime =
+                    LocalTime.now()                                                                        // Get local time from device
+                var d3: LocalTime =
+                    d1.plusHours(9)                                                             // Set Time plus 9 hr
+                var d4: LocalTime =
+                    d1.plusHours(8)                                                             // Set Time plus 8 hr
+
 /*
             Print all TextViews Here with logic
 */
-            var diffCompleted : Int = ChronoUnit.MINUTES.between(d1, d2).toInt()
-            var completedHours = diffCompleted / 60 //since both are ints, you get an int
-            var completedMinutes = diffCompleted % 60
+                var diffCompleted: Int = ChronoUnit.MINUTES.between(d1, d2).toInt()
+                var completedHours = diffCompleted / 60 //since both are ints, you get an int
+                var completedMinutes = diffCompleted % 60
 
-            timeCompletedTextView.text = completedHours.toString() + "hr " + completedMinutes.toString() + "min"        // Print Completed time: (d2 - d1)
-            timeCompletedHomeTextView.text =  d2.hour.toString()                                                        // Print System current time: LocalTime HH
-            timeCompletedHomeMinTextView.text =  d2.minute.toString()                                                   // Print System Completed time current time: LocalTime MM
+                timeCompletedTextView.text =
+                    completedHours.toString() + "hr " + completedMinutes.toString() + "min"        // Print Completed time: (d2 - d1)
+                timeCompletedHomeTextView.text =
+                    d2.hour.toString()                                                        // Print System current time: LocalTime HH
+                timeCompletedHomeMinTextView.text =
+                    d2.minute.toString()                                                   // Print System Completed time current time: LocalTime MM
 
-            var diffRem9 : Int = ChronoUnit.MINUTES.between(d2, d3).toInt()
-            var rem9Hours = diffRem9 / 60
-            var rem9Minuutes = diffRem9 % 60
+                var diffRem9: Int = ChronoUnit.MINUTES.between(d2, d3).toInt()
+                var rem9Hours = diffRem9 / 60
+                var rem9Minuutes = diffRem9 % 60
 
-            timeRemf9TextView.text = rem9Hours.toString() + "hr " + rem9Minuutes.toString() + "min"                     // Print Remaining time for 9hr: (d3 - d2)
-            timeRemf9HomeTextView.text =  d3.hour.toString()                                                            // Print Final time for 9hr: d1 + 9hr HH
-            timeRemf9HomeMinTextView.text =  d3.minute.toString()                                                       // Print Final time for 9hr: d1 + 9hr MM
+                timeRemf9TextView.text =
+                    rem9Hours.toString() + "hr " + rem9Minuutes.toString() + "min"                     // Print Remaining time for 9hr: (d3 - d2)
+                timeRemf9HomeTextView.text =
+                    d3.hour.toString()                                                            // Print Final time for 9hr: d1 + 9hr HH
+                timeRemf9HomeMinTextView.text =
+                    d3.minute.toString()                                                       // Print Final time for 9hr: d1 + 9hr MM
 
-            var diffRem8 : Int = ChronoUnit.MINUTES.between(d2, d4).toInt()
-            var rem8Hours = diffRem8 / 60
-            var rem8Minutes = diffRem8 % 60
+                var diffRem8: Int = ChronoUnit.MINUTES.between(d2, d4).toInt()
+                var rem8Hours = diffRem8 / 60
+                var rem8Minutes = diffRem8 % 60
 
-            timeRemf8TextView.text = rem8Hours.toString() + "hr " + rem8Minutes.toString() + "min"                      // Print Remaining time for 8hr: (d4 - d2)
-            timeRemf8HomeTextView.text =  d4.hour.toString()                                                            // Print Final time for 8hr: d1 + 8hr HH
-            timeRemf8HomeMinTextView.text =  d4.minute.toString()                                                       // Print Final time for 8hr: d1 + 8hr MM
+                timeRemf8TextView.text =
+                    rem8Hours.toString() + "hr " + rem8Minutes.toString() + "min"                      // Print Remaining time for 8hr: (d4 - d2)
+                timeRemf8HomeTextView.text =
+                    d4.hour.toString()                                                            // Print Final time for 8hr: d1 + 8hr HH
+                timeRemf8HomeMinTextView.text =
+                    d4.minute.toString()                                                       // Print Final time for 8hr: d1 + 8hr MM
 /*
             Change color of TextView when completed the time
 */
-            if (diffCompleted > 540) {
-                timeCompletedTextView.setTextColor(this.getResources().getColor(R.color.green))
-            }
+                if (diffCompleted > 540) {
+                    timeCompletedTextView.setTextColor(this.getResources().getColor(R.color.green))
+                }
 
-            if (diffRem9 < 0){
-                timeRemf9TextView.setTextColor(getColor(R.color.green))
-            }
+                if (diffRem9 < 0) {
+                    timeRemf9TextView.setTextColor(getColor(R.color.green))
+                }
 
-            if (diffRem8 < 0){
-                timeRemf8TextView.setTextColor(getColor(R.color.green))
-            }
+                if (diffRem8 < 0) {
+                    timeRemf8TextView.setTextColor(getColor(R.color.green))
+                }
 
+            }
+        }
+
+        buttonSetting.setOnClickListener {
+            val intent = Intent(this, ConfigActivity::class.java)
+            startActivity(intent)
         }
     }
 }
